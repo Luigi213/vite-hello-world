94432cc419340247295e8fbfe29481370e27e391
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-10 13:21:03 +0100

    start

---
+++ src/components/appTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data(){
+    return{
+      hello: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<template lang="">
+  <div>
+    {{hello}}
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
