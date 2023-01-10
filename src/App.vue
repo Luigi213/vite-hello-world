d273e37d1d5faebde7ec8b3e93e0df17e9113bf0
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-10 13:26:47 +0100

    completed the basic and bonus exercises

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script>
   import appTitle from './components/appTitle.vue';
+  import appHero from './components/appHero.vue';
   export default {
     components:{
-      appTitle
+      appTitle,
+      appHero
     }
   }
 </script>
 
 <template>
-  <appTitle></appTitle>
+  <div>
+    <appTitle></appTitle>
+    <appHero></appHero>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/appHero.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template lang="">
+    <div>
+        <img src="https://mdbcdn.b-cdn.net/wp-content/uploads/2017/08/jumbotron.jpg">
+    </div>
+</template>
+
+<style>
+
+img{
+    width: 200px;
+    height: 200px;
+}
+
+</style>
